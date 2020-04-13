65d2f25e505ac3508a1438ccdb9b6d2f5da28004
arteboris <arteboris@ukr.net>
2020-04-13 16:48:40 +0300

    Updates

---
+++ precache-manifest.59254b932969a47704012171d7ccdea4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01bf48c7c437109240ef312a3936bcd",
+    "url": "/appco/index.html"
+  },
+  {
+    "revision": "f2eb6ae8664cba9cdc11",
+    "url": "/appco/static/css/main.bd4e0979.chunk.css"
+  },
+  {
+    "revision": "bd9d530bf26650b3e8ba",
+    "url": "/appco/static/js/2.c3be2662.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/appco/static/js/2.c3be2662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2eb6ae8664cba9cdc11",
+    "url": "/appco/static/js/main.1a4cfd8e.chunk.js"
+  },
+  {
+    "revision": "09bba1a391a5887b75da",
+    "url": "/appco/static/js/runtime-main.20b499a6.js"
+  },
+  {
+    "revision": "117eba7b2b7955cc480d0535b530c110",
+    "url": "/appco/static/media/Rectangle.117eba7b.png"
+  },
+  {
+    "revision": "97b707d598170f77535d1ff267157f44",
+    "url": "/appco/static/media/Rectangle8.97b707d5.png"
+  },
+  {
+    "revision": "8434b9add409ff04d51dd1b0e41700e5",
+    "url": "/appco/static/media/clean.8434b9ad.png"
+  },
+  {
+    "revision": "6b8585130aab956512f87832e563c9ee",
+    "url": "/appco/static/media/footer.6b858513.png"
+  },
+  {
+    "revision": "f4f9e5768dddf1bdccbaca7247fc188e",
+    "url": "/appco/static/media/macbook.f4f9e576.png"
+  },
+  {
+    "revision": "a65ae3656e38d43e68116e6ddaf58449",
+    "url": "/appco/static/media/mobile.a65ae365.png"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/appco/static/media/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/appco/static/media/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "5cad650422a7184467af5a4d17b264c4",
+    "url": "/appco/static/media/montserrat-v14-latin-italic.5cad6504.woff2"
+  },
+  {
+    "revision": "d191f22af3bb50902b99ac577f81a322",
+    "url": "/appco/static/media/montserrat-v14-latin-italic.d191f22a.woff"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/appco/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/appco/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "9438b199d123e9fe3a0003cd898d1b06",
+    "url": "/appco/static/media/secure.9438b199.png"
+  }
+]);
